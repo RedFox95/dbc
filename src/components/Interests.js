af6f2bba954ad677698593cfc9bd83487143f241
Ryan Weidling <ryanweidling@gmail.com>
2024-08-08 21:23:39 -0400

    fixed border radius and interests paragraph

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ export default function Interests() {
     return (
         <div className='interests'>
             <h1>Interests</h1>
-            <p>Golfing, traveling, gaming (Fortnite), playing with my two favorite dogs (Star + Romeo), and lifting</p>
+            <p>Golfing, traveling, gaming (Fortnite), playing with my two favorite dogs (Star + Romeo), and lifting!</p>
         </div>
     )
 }
